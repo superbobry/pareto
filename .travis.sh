30a0d43addf703cd908fd1618da150fd46d9c3b1
Sergei Lebedev <superbobry@gmail.com>
2013-06-18 13:35:23 +0400

    Trying out Travis CI configuration
      See http://blog.mlin.net/2013/02/testing-ocaml-projects-on-travis-ci.html
      for detail

---
+++ .travis.sh
@@ -0,0 +1,32 @@
+# Hacking the build into Travis-CI "C" environment
+# Inspired by
+#     http://blog.mlin.net/2013/02/testing-ocaml-projects-on-travis-ci.html
+
+DEBS=libgsl-dev
+
+# OPAM version to install:
+export OPAM_VERSION=1.0.0
+
+# OPAM packages needed to build tests:
+export OPAM_PACKAGES='ocamlfind gsl oUnit'
+
+# Install OCaml
+sudo apt-get update -q -y
+sudo apt-get install -q -y ocaml ${DEBS}
+
+# Install OPAM
+curl -L https://github.com/OCamlPro/opam/archive/${OPAM_VERSION}.tar.gz \
+    | tar xz -C /tmp
+pushd /tmp/opam-${OPAM_VERSION}
+./configure
+make
+sudo make install
+opam init --auto-setup
+eval `opam config -env`
+popd
+
+# Install packages from OPAM repository.
+opam install -q -y ${OPAM_PACKAGES}
+
+# Compile & run tests
+./configure --enable-tests && make test
