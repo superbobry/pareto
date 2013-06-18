c97d153f8c8d8a58d360138cfcef7eb6cac7b895
Sergei Lebedev <superbobry@gmail.com>
2013-06-18 14:20:27 +0400

    Be quiet when intalling OCaml and don't ask questions

--- .travis.sh
+++ .travis.sh
@@ -16,8 +16,13 @@ sudo apt-get install -q -y ${DEBS}
 
 # Install OPAM
 wget http://www.ocamlpro.com/pub/opam_installer.sh
-sudo sh ./opam_installer.sh /usr/local/bin 4.00.1
-opam init --auto-setup
+
+# HACK(superbobry): make sure 'opam_installer.sh' won't initialized
+# OPAM for us.
+mkdir $HOME/.opam
+sudo sh ./opam_installer.sh /usr/local/bin
+rmdir $HOME/.opam
+opam init --comp=4.00.1 --auto-setup -q -y
 eval `opam config -env`
 
 # Install packages from OPAM repository.
